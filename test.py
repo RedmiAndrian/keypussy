25dad2c3faaaf8dcaebce3f2311973aec796b5f0
Andrian Harry Kenneth <63767615+RedmiAndrian@users.noreply.github.com>
2021-10-12 23:55:40 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ def _help():
     print("Type \"help\" to show this message.")
     print("Type \"read\" to show every password kept by keypussy")
     print("Type \"reset\" to delete every password kept by keypussy")
+    print("Type \"write\" to add password to keep by keypussy")
     print("Type \"about\" for this project's lore. lol")
     print("Type \"quit\" or \"exit\" to exit this utility")
     print()
@@ -23,17 +24,28 @@ def _help():
 
 def _read():
     print()
+    b = 1
+    c = 0
     f = open("password.txt", "r")
     content_list = f.read().split()
-    print(content_list)
+    d = int(len(content_list) / 3)
+    # print(content_list)
+    for i in range(d):
+        print(b, end=" ")
+        for j in range (3):
+            print(content_list[c + j], end =" ")
+        c = c + 3
+        b = b + 1
+        print()
     f.close()
     print()
 
 def _process():
     f = open("password.txt", "a")
+    platform = str(input("What platform will it be: "))
     email = str(input("What is your email/username: "))
     password = str(input("password to keep: "))
-    f.write(email + " " + password + " ")
+    f.write(platform + " " + email + " " + password + " ")
     f.close()
     print()
 
